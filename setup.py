daf57ebd77cf69e316f57aa09e8596414be0b2ea
Harvey Ormston <htormston@gmail.com>
2017-01-02 10:39:53 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+""" Setup osc_gen """
+
+from distutils.core import setup
+
+setup(name='osc_gen',
+      version='0.1',
+      description='Generate oscilator wavetoables',
+      author='Harvey Ormston',
+      author_email='htormston@gmail.com',
+      url='https://github.com/harveyormston/osc_gen',
+      packages=['osc_gen'])
